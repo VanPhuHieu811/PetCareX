924ba16aa5d987145eeef2c2722c1d49a03302dc
cuogne <cuog.nc116@gmail.com>
2026-01-08 22:15:44 +0700

    feat: register

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -26,3 +26,30 @@ export const login = async (pool, email, password) => {
     throw new Error(`Login failed: ${err.message}`);
   }
 }
+
+export const register = async (pool, email, password, name) => {
+  try {
+    const request = pool.request();
+
+    request.input('Email', email);
+    request.input('MatKhau', password);
+    request.input('HoTen', name);
+
+    const result = await request.execute('sp_register');
+
+    if (result.recordset && result.recordset.length > 0) {
+      return result.recordset[0];
+    }
+
+    return {
+      HoTen: name,
+      Email: email,
+      VaiTro: 'Khách hàng',
+      TrangThai: 'Hoạt động'
+    };
+
+  }
+  catch (err) {
+    throw new Error(`Registration failed: ${err.message}`);
+  }
+}
