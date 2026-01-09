600f39511a36ab84b5a2fa5c3a11864cc1c653b4
BrianNguyen2k5 <manhpl2019@gmail.com>
2026-01-09 22:54:11 +0700

    Add Vacxin BE

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,8 @@ import cors from 'cors';
 import branchRoutes from './routes/branch.route.js';
 import doctorRoutes from './routes/doctor.route.js';
 import { dbMiddleware } from './config/sqlserver.config.js';
-
-import branchRoutes from './routes/branch.route.js';
 import authRoutes from './routes/auth.route.js';
+import vacxinRoutes from './routes/vacxin.route.js';
 
 const app = express();
 
@@ -20,5 +19,6 @@ app.use(dbMiddleware);
 app.use('/api/v1/branches', branchRoutes);
 app.use('/api/v1/doctors', doctorRoutes);
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/vacxin', vacxinRoutes);
 
 export default app;
